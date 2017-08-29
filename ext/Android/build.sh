c0ea27d842a3d1d515a07d35329cc16d4ad82be0
uerceg <ugi@adjust.com>
2017-08-29 11:56:46 +0200

    Use gradle instead of mvn in Android SDK build script

--- ext/Android/build.sh
+++ ext/Android/build.sh
@@ -1,16 +1,27 @@
 #!/usr/bin/env bash
-
+# End script if one of the lines fails
 set -e
 
-MVNDIR=./sdk/Adjust
-JARINDIR=./sdk/Adjust/target
-JAROUTDIR=../../Assets/Adjust/Android
+# Get the current directory (ext/android/)
+SDK_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-(cd $MVNDIR; mvn clean)
-(cd $MVNDIR; mvn package)
+# Traverse up to get to the root directory
+SDK_DIR="$(dirname "$SDK_DIR")"
+SDK_DIR="$(dirname "$SDK_DIR")"
+BUILD_DIR=sdk/Adjust
+JAR_IN_DIR=adjust/build/intermediates/bundles/default
+JAR_OUT_DIR=Assets/Adjust/Android
 
-rm -v $JAROUTDIR/adjust-android*
-cp -v $JARINDIR/adjust-android-*.*.*.jar $JAROUTDIR
-rm -v $JAROUTDIR/*-javadoc.jar
-rm -v $JAROUTDIR/*-sources.jar
-mv -v $JAROUTDIR/adjust-android-*.*.*.jar $JAROUTDIR/adjust-android.jar
+RED='\033[0;31m' # Red color
+GREEN='\033[0;32m' # Green color
+NC='\033[0m' # No Color
+
+# cd to the called directory to be able to run the script from anywhere
+cd $(dirname $0)
+
+cd $BUILD_DIR
+echo -e "${GREEN}>>> Running Gradle tasks: clean clearJar makeJar ${NC}"
+./gradlew clean clearJar makeJar
+
+echo -e "${GREEN}>>> Moving the jar from ${JAR_IN_DIR} to ${JAR_OUT_DIR} ${NC}"
+mv -v ${JAR_IN_DIR}/*.jar ${SDK_DIR}/${JAR_OUT_DIR}/adjust-android.jar
